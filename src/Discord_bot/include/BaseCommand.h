60b56b562ccd950a6a55406ec80249918bafba87
MrSteelRat <drebin341@gmail.com>
2024-10-20 23:15:21 +0300

    Add Discord bot configs

---
+++ src/Discord_bot/include/BaseCommand.h
@@ -0,0 +1,19 @@
+#ifndef BASE_COMMAND_BOT_H
+#define BASE_COMMAND_BOT_H
+
+#include <iostream>
+#include <string>
+
+class BaseCommand
+{
+public:
+    BaseCommand();
+
+    void base_command();
+
+    ~BaseCommand();
+
+private:
+};
+
+#endif  // BASE_COMMAND_BOT_H
---
+++ src/Discord_bot/include/Core.h
@@ -0,0 +1,19 @@
+#ifndef CORE_H
+#define CORE_H
+
+class Core
+{
+public:
+    Core(/* args */)
+    {
+    }
+
+    ~Core()
+    {
+    }
+
+private:
+    /* data */
+};
+
+#endif // CORE_H
